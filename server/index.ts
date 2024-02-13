992edffc7eece84217fbe446b4db7ca8e631d3c1
javier-rosas <jr2dj@virginia.edu>
2024-02-13 15:37:25 -0500

    fix: changed port

--- server/index.ts
+++ server/index.ts
@@ -53,5 +53,5 @@ io.on("connection", (socket) => {
   );
 });
 
-const PORT: number | string = process.env.PORT || 5000;
+const PORT: number | string = process.env.PORT || 2000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
